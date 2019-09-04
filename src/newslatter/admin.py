435937f74738e498f5fe0acb9795791308adf689
timy116 <timy117@gmail.com>
2019-09-04 23:49:07 +0800

    Register newslatter to admin

--- src/newslatter/admin.py
+++ src/newslatter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from newslatter.models import SignUp
 
-# Register your models here.
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ['email', 'full_name', 'timestamp', 'update']
+
+    class Meta:
+        model = SignUp
+
+
+admin.site.register(SignUp, SignUpAdmin)
