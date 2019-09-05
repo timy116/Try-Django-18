d701f3ae893fc1e37701fa385c28372892a907fe
timy116 <timy117@gmail.com>
2019-09-06 00:25:52 +0800

    Add views.index func

--- src/newslatter/forms.py
+++ src/newslatter/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from newslatter.models import SignUp
 
@@ -7,3 +8,9 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['email', 'full_name']
+
+    def clean_email(self):
+        email = self.data.get('email')
+        if not re.match(r'[a-z][\w]{5,32}@[a-z\d]{2,}([\.]{1,}[a-z0-9]{2,}){1,}', email):
+            return forms.ValidationError('Email format error.')
+        return email
