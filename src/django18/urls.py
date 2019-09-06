50cb535607044f7b70e9eac314ff3d939d8ae307
timy116 <timy117@gmail.com>
2019-09-06 11:56:50 +0800

    Add static and media in urls

--- src/django18/urls.py
+++ src/django18/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^newsletter/', include('newsletter.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
