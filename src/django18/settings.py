21562d115f7f68fa3238c864508c920e59d311e0
timy116 <timy117@gmail.com>
2019-09-06 11:42:05 +0800

    Add static dir and static_cdn dir and media root

--- src/django18/settings.py
+++ src/django18/settings.py
@@ -103,3 +103,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
